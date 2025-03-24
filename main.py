3c1fbb5096a639dec06b0f5a5315ba38715cb145
Orles24 <Orles24@mail.ru>
2025-03-24 20:57:19 +0700

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-from flask import Flask
-from threading import Thread
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from keep_alive import keep_alive
 import os
 import random
 
-# Токен и путь к папке
-TOKEN = "7973405224:AAGLcKbvra7wX4Jf8loiflTRqD-CcnBHsWs"
+TOKEN = os.getenv("TOKEN")
 CARDS_DIR = "cards"
 cards = [os.path.join(CARDS_DIR, f) for f in os.listdir(CARDS_DIR) if f.endswith(".jpg")]
+
 user_draw_counts = {}
 
-# Flask для «оживления» бота на Replit
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-# Команды Telegram
 def start(update: Update, context: CallbackContext):
     keyboard = [[InlineKeyboardButton("🔮 Вытянуть карту", callback_data="draw_card")]]
     reply_markup = InlineKeyboardMarkup(keyboard)
@@ -34,7 +18,7 @@ def start(update: Update, context: CallbackContext):
         "основанной на реальных локациях Алтая.\n\n"
         "Каждая карта — это не просто изображение, а символ, отклик, подсказка, "
         "возможность услышать ответ и задуматься о жизни 💭\n\n"
-        "Нажимай на кнопку и смотри, что хочет сказать тебе Алтай ⬇️",
+        "Нажимай на кнопку и смотри, что хочет сказать тебе Алтай.\n\n👇",
         reply_markup=reply_markup
     )
 
@@ -44,14 +28,17 @@ def draw_card(update: Update, context: CallbackContext):
     user_id = query.from_user.id
     user_draw_counts[user_id] = user_draw_counts.get(user_id, 0) + 1
     count = user_draw_counts[user_id]
+
     card_path = random.choice(cards)
 
     keyboard = [
         [InlineKeyboardButton("🔮 Вытянуть ещё", callback_data="draw_card")],
         [InlineKeyboardButton("☁️ Связаться с автором", url="https://t.me/Orles_24")]
     ]
+
     if count % 5 == 0:
         keyboard.append([InlineKeyboardButton("☕ Поблагодарить автора", callback_data="thank_author")])
+
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     with open(card_path, 'rb') as photo:
